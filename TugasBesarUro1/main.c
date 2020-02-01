90247a5ce3a27a58ef88a5d1549e581f1c5e0021
abilcode <m.nabilfad@gmail.com>
2020-02-01 10:16:47 +0700

    Update main.c
    Membuat Flow Program dengan Switch Case, If statment, dan while Muhammad Nabil Fadhlurrahman {16019025}

--- TugasBesarUro1/main.c
+++ TugasBesarUro1/main.c
@@ -1,24 +1,85 @@
-/*
- * main.c
- *
- *  Created on: Feb 1, 2020
- *      Author: Niveus Caelus
- */
-
 #include "gamerules.h"
 
 int main()
 {
-	int x;
-	Statistik S1;/*Membuat variable data struct*/
+	int MilihKegiatan;
+	/*Membuat variable data struct*/
+	Statistik 
+
+	S1;
 	Achtung();/*GUI*/
 	Garis();
 
 	StartSetZero(&S1);/*Set Status awal robot*/
-	TampilanStat(S1);/*Menampilkan status awal robot*/
+	;/*Menampilkan status awal robot*/
+	//UTS(&S1);/*Ambis kuy*/
+	
+	//Input Kegiatan :
+	
+		
+	do
+	{
+		printf("Masukkan Kegiatan: ");
+		scanf("%d", &MilihKegiatan);
+		switch (MilihKegiatan)
+		{
+			case 0 :
+				TampilanStat(S1);
+				break;
+			case 1 :
+				Tidur(&S1);
+				break;
+			case 2 :
+				Play(&S1);
+				break;
+			case 3:
+				Work (&S1);
+				break;
+			case 4 :
+				Pacaran (&S1);
+				break;
+			case 5 :
+				Workout(&S1);
+				break;
+			case 6 :
+				UTS(&S1);
+				break;
+			case 7 :
+				Up(&S1);
+				break;
+			case 8 :
+				S1.health = 0;
+				break;
+			case 9 :
+				Danus(&S1);
+				break;
+			case 10 :
+				Gamais(&S1);
+				break;
+			case 11 :
+				Uro(&S1);
+				break;
+			default :
+				printf("Input Tidak valid\n");
+				Garis();
 
-	printf("Masukkan nilai 1: ");
-	scanf("d", &x);
-	UTS(&S1);/*Ambis kuy*/
-	TampilanStat(S1);
+		}
+	}while(S1.health > 0 && S1.health <= 100 );
+	
+	if(S1.health <= 0)
+	{
+		S1.health = 0;
+		TampilanStat(S1);
+		printf("You Lose!\n" );
+
+	}
+	else
+	{
+		S1.health = 100;
+		TampilanStat(S1);
+		printf("You Win!\n");
+
+	}
+
+	return 0;
 }
